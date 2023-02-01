b7af248cdca75dc4c665fdecc388b73e8f2c90a8
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2023-02-01 20:01:14 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,7 +20,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Sanzuu
+${htjava} *💌 Nama* : Bang el
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
